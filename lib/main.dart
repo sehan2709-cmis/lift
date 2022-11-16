88d156f8a8cd1c1165ff6f1a0ad7871afc4d6e75
Eunhyeok Kwon <eunhyeoq@gmail.com>
2022-11-16 21:33:30 +0900

    import firebase_core, firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'app.dart';
+
 
 void main() {
   runApp(const App());
