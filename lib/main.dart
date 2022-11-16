49de7d8a0c85ef81a4c0de0b64b7853cfdecbb2b
Eunhyeok Kwon <eunhyeoq@gmail.com>
2022-11-16 21:58:38 +0900

    Make App as ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
-
 import 'app.dart';
+import 'model/ApplicationState.dart';
 
 
 void main() {
-  runApp(const App());
+  // runApp(const App());
 
-  /*WidgetsFlutterBinding.ensureInitialized();
-
-  // runApp(const ShrineApp());
+  WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
-    // StateManagement using Provider class
-    // ChangeNotifierProvider is initialized at the root(main) of this application
-    // ApplicationState will be available for the entire application
-    // Just need put the Widgets inside the Consumer<ApplicationState>'s builder parameter to make it listen to the state changes
-    ChangeNotifierProvider(
+   ChangeNotifierProvider(
       create: (context) => ApplicationState(),
-      builder: ((context, child) => const FinalApp()),
+      builder: ((context, child) => const App()),
     )
-  );*/
+  );
 }
