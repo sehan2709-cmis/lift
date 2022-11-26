ff630286ca741e83f0e468d4e76865ee20efbe0e
Eunhyeok Kwon <eunhyeoq@gmail.com>
2022-11-26 18:38:54 +0900

    Create WorkoutState and Workout data model

---
+++ lib/model/Workout.dart
@@ -0,0 +1,28 @@
+class Workout {
+  Workout(this.workout);
+
+  // field is not fixed value
+  final workout;
+
+  // data structure
+  /*
+  [
+    {'name1':[
+      {
+        'weight':10,
+        'reps':9,
+      },
+      {
+      'weight':10,
+      'reps':9,
+      },
+    ],
+    'name2':[
+      {
+        'weight':20,
+        'reps':5,
+      },
+    ]},
+  ]
+  */
+}
