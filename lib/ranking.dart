2d0ccb1a6938119bd0b79d8b43e2799e23f19631
Eunhyeok Kwon <eunhyeoq@gmail.com>
2022-12-01 22:44:31 +0900

    Display ranking for all 3 tabs: volume, sbdSum, streak. And add more decoration.

--- lib/ranking.dart
+++ lib/ranking.dart
@@ -28,21 +28,97 @@ class _RankingState extends State<Ranking>
     Future.delayed(Duration.zero, () async {
       /// 아직 context가 만들어 지지 않았는데 context의 provider를 접근하려고 하는게 문제가 생기는 것 같다
       /// delayed에 이렇게 넣으면 build가 끝난 다음에 실행되기 때문에 괜찮다
-      Provider.of<WorkoutState>(context, listen: false).downloadVolumeRanking();
-      log("downloaded");
+      WorkoutState simpleWorkoutState =
+          Provider.of<WorkoutState>(context, listen: false);
+      simpleWorkoutState.downloadVolumeRanking();
+      simpleWorkoutState.downloadStreakRanking();
+      simpleWorkoutState.downloadSBDSumRanking();
+      log("RANK :: downloaded rankings");
     });
+
+    List<Widget> _rankingBuilder(
+      Map<String, List<String>> rankData,
+      int dataSize,
+    ) {
+      List<Widget> ranks = [];
+      ranks.add(SizedBox(
+        height: 15,
+      ));
+      //workoutState.volumeRankingSize
+      for (var i = 0; i < dataSize; i++) {
+        Widget one = Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 5,
+            horizontal: 20,
+          ),
+          child: InkWell(
+            onTap: (){
+              /// go to global user profile page
+              /// + Hero animation
+            },
+            // highlightColor: Colors.black,
+            // splashColor: Colors.blue,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4.5),
+                border: Border.all(width: 1.0),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                      height: 50,
+                      width: 30,
+                      child: Center(
+                        child: Text("${i + 1}"),
+                      )),
+                  SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: ClipOval(
+                      /// need to change the image to display user image
+                      child: Image.network(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRFU7U2h0umyF0P6E_yhTX45sGgPEQAbGaJ4g&usqp=CAU',
+                          fit: BoxFit.cover),
+                      clipper: MyClip(),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Text(
+                      "${rankData["user"]?.elementAt(i)}",
+                      // "${workoutState.volumeRanking["user"]?.elementAt(i)}"
+                    ),
+                  ),
+                  Flexible(
+                    flex: 5,
+                    child: Text(
+                      "${rankData["score"]?.elementAt(i)}",
+                      // "${workoutState.volumeRanking["totalVolume"]?.elementAt(i)}"
+                    ),
+                  ),
+                  SizedBox(
+                    width: 7,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+        ranks.add(one);
+      }
+      return ranks;
+    }
 
     return DefaultTabController(
       length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: Text("Ranking"),
-          bottom: PreferredSize(
+          bottom: const PreferredSize(
             preferredSize: Size.fromHeight(20),
             child: TabBar(
-              // indicator: BoxDecoration(
-              //     borderRadius: BorderRadius.circular(50), // Creates border
-              //     color: Colors.greenAccent),
               indicatorWeight: 6,
               tabs: [
                 Text("Volume"),
@@ -56,41 +132,44 @@ class _RankingState extends State<Ranking>
           builder: (context, workoutState, _) => TabBarView(
             children: [
               ListView(
-                children: [
-                  // variable i need to keep increasing, so can't use final keyword here
-                  for (var i = 0; i < workoutState.volumeRankingSize; i++)
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                              width: 50,
-                              child: Center(
-                                child: Text("${i + 1}"),
-                              )),
-                          Expanded(
-                            flex: 5,
-                            child: Text(
-                                "${workoutState.volumeRanking["user"]?.elementAt(i)}"),
-                          ),
-                          Flexible(
-                            child: Text(
-                                "${workoutState.volumeRanking["totalVolume"]?.elementAt(i)}"),
-                          ),
-                        ],
-                      ),
-                    ),
-                ],
+                children:
+                    // variable i need to keep increasing, so can't use final keyword here
+                    _rankingBuilder(
+                  workoutState.volumeRanking,
+                  workoutState.volumeRankingSize,
+                ),
               ),
-              Text("1"),
-              Text("2"),
+              ListView(
+                children:
+                    // variable i need to keep increasing, so can't use final keyword here
+                    _rankingBuilder(
+                  workoutState.sbdSumRanking,
+                  workoutState.sbdSumRankingSize,
+                ),
+              ),
+              ListView(
+                children:
+                    // variable i need to keep increasing, so can't use final keyword here
+                    _rankingBuilder(
+                  workoutState.streakRanking,
+                  workoutState.streakRankingSize,
+                ),
+              ),
             ],
           ),
         ),
         bottomNavigationBar: BNavigationBar(),
       ),
     );
+  }
+}
+
+class MyClip extends CustomClipper<Rect> {
+  Rect getClip(Size size) {
+    return Rect.fromLTWH(0, 7, 36, 36);
+  }
+
+  bool shouldReclip(oldClipper) {
+    return false;
   }
 }
